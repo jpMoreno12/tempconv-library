fc7efe11cae3d52d347a7b6fc4bfd9308abe5415
morendev <morenodasilvajoao709@gmail.com>
2025-06-27 14:03:43 -0300

    Initial Release

---
+++ src/TemperatureConverter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace jpMoreno12\TempConv;
+
+class TemeperatureConverter
+{
+
+    public function convert($temperature, $unit) {
+
+        if ($unit === 'C') {
+            $fahrenheit = ($temperature * 9/ 5) + 32;
+            return $fahrenheit;
+        } elseif ($unit === 'F'){
+            $celsius = ($temperature - 32)  * 5 / 9;
+            return $celsius;
+        } else {
+            throw new \InvalidArgumentException('Invalid unit, only C (Celsius) or F (Fahrenheit) are allowed'); 
+        }
+    }
+}
